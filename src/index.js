3048e10c1611d1f42de8c442f3693fad5fab1658
Double-Domination <squremeta@gmail.com>
2021-08-20 22:48:26 +0700

    feat: task completed

--- src/index.js
+++ src/index.js
@@ -1,46 +1,68 @@
 const MORSE_TABLE = {
-    '.-':     'a',
-    '-...':   'b',
-    '-.-.':   'c',
-    '-..':    'd',
-    '.':      'e',
-    '..-.':   'f',
-    '--.':    'g',
-    '....':   'h',
-    '..':     'i',
-    '.---':   'j',
-    '-.-':    'k',
-    '.-..':   'l',
-    '--':     'm',
-    '-.':     'n',
-    '---':    'o',
-    '.--.':   'p',
-    '--.-':   'q',
-    '.-.':    'r',
-    '...':    's',
-    '-':      't',
-    '..-':    'u',
-    '...-':   'v',
-    '.--':    'w',
-    '-..-':   'x',
-    '-.--':   'y',
-    '--..':   'z',
-    '.----':  '1',
-    '..---':  '2',
-    '...--':  '3',
-    '....-':  '4',
-    '.....':  '5',
-    '-....':  '6',
-    '--...':  '7',
-    '---..':  '8',
-    '----.':  '9',
-    '-----':  '0',
+  '.-': 'a',
+  '-...': 'b',
+  '-.-.': 'c',
+  '-..': 'd',
+  '.': 'e',
+  '..-.': 'f',
+  '--.': 'g',
+  '....': 'h',
+  '..': 'i',
+  '.---': 'j',
+  '-.-': 'k',
+  '.-..': 'l',
+  '--': 'm',
+  '-.': 'n',
+  '---': 'o',
+  '.--.': 'p',
+  '--.-': 'q',
+  '.-.': 'r',
+  '...': 's',
+  '-': 't',
+  '..-': 'u',
+  '...-': 'v',
+  '.--': 'w',
+  '-..-': 'x',
+  '-.--': 'y',
+  '--..': 'z',
+  '.----': '1',
+  '..---': '2',
+  '...--': '3',
+  '....-': '4',
+  '.....': '5',
+  '-....': '6',
+  '--...': '7',
+  '---..': '8',
+  '----.': '9',
+  '-----': '0',
 };
 
 function decode(expr) {
-    // write your solution here
-}
+  const matcher = new RegExp(/(10)|(11)/, 'g');
+  let splitted = [];
+  let converted = [];
+  let input = expr;
+  //split in by 10
 
-module.exports = {
-    decode
+  for (let i = 0; i < expr.length; i += 10) {
+    splitted.push(input.substring(i, 10 + i));
+  }
+
+  for (currentEncodedCharcater of splitted) {
+    if (currentEncodedCharcater === '**********') {
+      converted.push(' ');
+    } else {
+      let matched;
+      matched = currentEncodedCharcater.replace(/(10)/g, '.');
+      matched = matched.replace(/(11)/g, '-');
+      matched = matched.replace(/0/g, '');
+      matched = MORSE_TABLE[matched];
+      converted.push(matched);
+    }
+  }
+
+  return converted.join('');
 }
+module.exports = {
+  decode,
+};
